46287c30bf387758733e50db8e16e79122f902c5
Levi Noppers <levinoppers@proton.me>
2025-10-27 16:26:30 +0100

    Fix: export new response at root

--- gecho.go
+++ gecho.go
@@ -6,13 +6,19 @@ import (
 	"github.com/MonkyMars/gecho/servererrors"
 )
 
+// Exported pkg Functions
 var Err = pkg.Err
 var OK = pkg.OK
 
+// Exported Client Error Functions
 var BadRequest = clienterrors.BadRequest
 var Unauthorized = clienterrors.Unauthorized
 var Forbidden = clienterrors.Forbidden
 var NotFound = clienterrors.NotFound
 
+// Exported Server Error Functions
 var InternalServerError = servererrors.InternalServerError
 var ServiceUnavailable = servererrors.ServiceUnavailable
+
+// Exported Types
+type NewResponse = pkg.NewResponse
